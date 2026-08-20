fbdfa562674a6758b1907b7b08b8d136eacf6b12
MuhammadAayanMirza <mmaayanmirza@gmail.com>
2026-08-20 19:11:04 +0500

    Completed making the movements script fro the player

--- Assets/Scripts/playerMovements.cs
+++ Assets/Scripts/playerMovements.cs
@@ -1,20 +1,38 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class playerMovements : MonoBehaviour
 {
    public float moveSpeed = 5f;
 
    private Rigidbody2D rb;
-   private Vector2 movement;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-
-    void Update()
+    void FixedUpdate()
     {
+        Vector2 movement = Vector2.zero;
+
+        if(Keyboard.current.wKey.isPressed || Keyboard.current.upArrowKey.isPressed)
+            movement.y += 1;
         
+        if(Keyboard.current.sKey.isPressed || Keyboard.current.downArrowKey.isPressed)
+            movement.y -= 1;
+
+        if(Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed)
+            movement.x += 1;
+
+        if(Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed)
+            movement.x -= 1;
+
+        movement = movement.normalized;
+
+        rb.linearVelocity = movement * moveSpeed;
+
     }
+
+
 }
